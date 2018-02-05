7b689bffbf0ddcbf5428b9e0e3008d8764c4e656
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-02-05 16:04:03 +0800

    Android和Ios 分别重置dialog样式

--- src/common/js/dialog.js
+++ src/common/js/dialog.js
@@ -1,9 +1,26 @@
-const dialogConf = {
-        background:'rgba(0,0,0,0.75)',
-        color:'#ffffff',
-        fontSize:'28px',
-        textAlign:'center',
-        lineHeight:'1.8125'
+function adustFontSize() {
+	let isAndroid = (/android/gi).test(window.navigator.appVersion)
+	let dpr = window.devicePixelRatio
+	console.log(isAndroid)
+	if(isAndroid) {
+		
+		return {
+			background:'rgba(0,0,0,0.75)',
+			color:'#ffffff',
+			fontSize:'16px',
+			textAlign:'center',
+			lineHeight:'1.8125'
+		}
+		
+	}else{
+		return {
+			background:'rgba(0,0,0,0.75)',
+			color:'#ffffff',
+			fontSize:'0.38rem',
+			textAlign:'center',
+			lineHeight:'1.8125'
+		}
+	}
 }
-
+const dialogConf = adustFontSize()
 export default dialogConf
